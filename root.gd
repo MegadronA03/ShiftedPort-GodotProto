e11c9ac999e2ce5c5d8c918b8a529e268e120d00
MegadronA03 <megadron.b@gmail.com>
2022-09-27 01:21:12 +0500

    added test scene

--- root.gd
+++ root.gd
@@ -6,8 +6,19 @@ func _ready():
 	var ctrlwnd = Console.new() #TODO: into external container
 	ctrlwnd.set_name("wnd")
 	add_child(ctrlwnd)
-	var t
-	var c
+	#var t
+	#var c
+	
+	#var w = WorldEnvironment.new()
+	#add_child(w)
+	#t = Camera3D.new()
+	#t.keep_aspect = Camera3D.KEEP_WIDTH
+	#w.add_child(t)
+	#t = MeshInstance3D.new()
+	#t.position = Vector3(-.75, -.75, -3.)
+	#t.mesh = PlaneMesh.new()
+	#w.add_child(t)
+	
 	#t = SubViewportContainer.new()
 	#t.anchor_top = 0.0
 	#t.anchor_left = 0.0
@@ -16,22 +27,17 @@ func _ready():
 	#t.stretch = true
 	#add_child(t)
 	#c = t
-	t = Window.new()
-	t.transparent_bg = true
-	t.size = floor(size*0.5)
-	t.position = size*0.25
+	
+	#t = Window.new()
+	#t.transparent_bg = true
+	#t.size = floor(size*0.5)
+	#t.position = size*0.25
 	#t.render_target_update_mode = SubViewport.UPDATE_ALWAYS
-	add_child(t)
-	c = t
-	var w = WorldEnvironment.new()
-	c.add_child(w)
-	t = Camera3D.new()
-	t.keep_aspect = Camera3D.KEEP_WIDTH
-	w.add_child(t)
-	t = MeshInstance3D.new()
-	t.position = Vector3(-.75, -.75, -3.)
-	t.mesh = PlaneMesh.new()
-	w.add_child(t)
+	#add_child(t)
+	#c = t
+	#t = Camera3D.new()
+	#t.keep_aspect = Camera3D.KEEP_WIDTH
+	#c.add_child(t)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
