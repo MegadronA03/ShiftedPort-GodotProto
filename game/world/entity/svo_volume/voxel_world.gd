fc35fb112e4a4020cea43542093c947f5576eb71
MegadronA03 <megadron.b@gmail.com>
2022-10-04 00:55:54 +0500

    started working on material palette

---
+++ game/world/entity/svo_volume/voxel_world.gd
@@ -0,0 +1,17 @@
+class_name VoxelWorld
+extends Node3D
+
+@onready var triobj = TriangData.new()
+
+var materials : Array = [
+	
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
