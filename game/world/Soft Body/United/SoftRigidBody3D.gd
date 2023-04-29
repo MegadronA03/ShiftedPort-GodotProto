081f2eb28ad0ebb5a78f3260f7f06a8dc872c007
MegadronA03 <megadron.b@gmail.com>
2023-04-30 01:12:25 +0500

    FEM contributions

--- game/world/Soft Body/United/SoftRigidBody3D.gd
+++ game/world/Soft Body/United/SoftRigidBody3D.gd
@@ -4,12 +4,30 @@ extends RigidBody3D
 const TetrahShape = preload("res://game/world/Soft Body/UnitTetrahedron.tscn")
 
 @export var mesh_shell : ArrayMesh #visual representation
-@export var points : PackedVector3Array #array with body verticies
+@export var point := {
+	"position" : PackedVector3Array(),
+	"velocity" : PackedVector3Array(),
+	"force" : PackedVector3Array(),
+	"mass" : PackedFloat32Array()
+}
+
+#palette with cached springs properities,
+# pulled from material properity palette. memo:(temperature, stress, strain)
+var springs : PackedVector3Array #array with springs (pi0, pi1, properities)
+var spring_prop := {
+	"stiffness" : PackedFloat32Array(),
+	"damping_f" : PackedFloat32Array(),
+	"drag_f" : PackedFloat32Array(),
+	"rest_length" : PackedFloat32Array(),
+	"rest_dislocation" : PackedFloat32Array(),
+} 
+
 @onready var parts : Array # shape shifting convex collision shapes
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	custom_integrator = false
+	#self.mass = sum of FEM masses
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +38,5 @@ func _physics_process(delta):
 	pass # shapeshifting call
 
 func _integrate_forces(state):
+	#Euler integration
 	pass
--- game/world/Soft Body/United/SoftRigidSPart3D.gd
+++ game/world/Soft Body/United/SoftRigidSPart3D.gd
@@ -2,6 +2,7 @@ extends CollisionShape3D
 
 #const unit_ttrhdrn = preload("res://game/world/Soft Body/UnitTetrahedron.tres")
 @onready var neighbours := [null,null,null,null]
+@onready var vert_ind : PackedInt32Array = [0,0,0,0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
